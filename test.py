d1369073eca15aa87af35440b45c3f6cbc26ff4a
Marko Dankovich <markodankovic@protonmail.com>
2022-11-12 00:20:59 +0100

    second commit

--- test.py
+++ test.py
@@ -13,20 +13,14 @@ if calculation == 'n':
     payment = int(input())
     print('Enter the loan interest:')
     loan_interest_percentage = int(input())
-    interest = principal / loan_interest_percentage
-    p = 12 * principal
-    i = interest / p
-    x = principal * i
-    pv = x / payment
-    result = 1 - pv
-
-    n = math.log(result) / math.log(i)
-
-    print(n)
-
-elif calculation == 'a':
-    print('Enter the loan principal:')
-    principal = int(input())
-    print('Enter the number of periods:')
-    periods = int(input())
-    print('Enter the loan interest:')
+    i = loan_interest_percentage / 1200
+    
+    x = (payment / payment - i * principal)
+    base = 1 + i
+    print(math.log(base, x))
+    
+    '''for anyone whos struggling with the log function in example 1:
+    n = log 1 + i (A / A − i ∗ P)
+    math.log(x, base)
+    x = (A / A − i ∗ P)
+    base = 1 + i'''
