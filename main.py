e53029628660aa3a82507944ca3913955b619e29
Marko Dankovich <markodankovic@protonmail.com>
2022-11-15 12:22:40 +0100

    final commit

--- main.py
+++ main.py
@@ -1,28 +1,87 @@
 import math
+import argparse
 
-print('Enter the loan principal:')
-principal = int(input())
-print('What do you want to calculate?')
-print('type "m" for number of monthly payments,')
-print('type "p" for the monthly payment:')
-payments = input()
+def main():
 
-if payments == 'm':
-    print('Enter the monthly payment:')
-    monthly_payment = int(input())
-    months = principal / monthly_payment
-    if months == 1:
-        print(f"It will take {round(months)} month to repay the loan")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--type', metavar='type', type=str, help='enter the type of calculator')
+    parser.add_argument('--principal', type=int)
+    parser.add_argument('--payment', type=int)
+    parser.add_argument('--interest', type=float)
+    parser.add_argument('--periods', type=int)
+    args = parser.parse_args()
+
+    calculator_type = args.type
+    principal = args.principal
+    payment = args.payment
+    interest = args.interest
+    periods = args.periods
+
+
+    if calculator_type == 'diff' and args.principal and args.periods and args.interest:
+        diff_payment_calculator(principal, interest, periods)
+    elif calculator_type == 'annuity' and args.principal and args.periods and args.interest:
+        annuity_payment_calculator(principal, periods, interest)
+    elif calculator_type == 'annuity' and args.payment and args.periods and args.interest:
+        annuity_principal_calculator(payment, periods, interest)
+    elif calculator_type == 'annuity' and args.principal and args.payment and args.interest:
+        annuity_period_calculator(principal, payment, interest)
     else:
-        print(f"It will take {round(months)} months to repay the loan")
-elif payments == 'p':
-    print('Enter the number of months:')
-    months = int(input())
-    payment = principal / months
-    if isinstance(payment, float):
-        payment = math.ceil(payment)
-        last_payment = principal - (months - 1) * payment
-        print(f"Your monthly payment = {payment} and the last payment = {last_payment}.")
-    elif isinstance(payment, int):
-        print(f"Your monthly payment = {payment}")
+        print('Incorrect parameters')
+
+
+
+def diff_payment_calculator(principal, interest, periods):
+    i = interest / 1200
+    x = principal / periods
+    all_payments = 0
+    m = 1
+    while m <= periods:
+        y = principal - (principal * (m - 1) / periods)
+        result = math.ceil(x + i * y)
+        print(f"Month {m}: payment is {result}")
+        all_payments += result
+        m += 1
+    overpayment = all_payments - principal
+    print()
+    print(f"Overpayment = {overpayment}")
+
+def annuity_payment_calculator(principal, periods, interest):
+    i = interest / 1200
+    x = principal * i
+    y = math.pow(1 + i, periods)
+    m = math.ceil((x * y) / (y - 1))
+    overpayment = m * periods - principal
+    print(f"Your annuity payment = {m}!")
+    print(f'Overpayment = {overpayment}')
+
+
+def annuity_principal_calculator(payment, periods, interest):
+    i = interest / 1200
+    x = i * math.pow(i + 1, periods)
+    y = math.pow(1 + i, periods) - 1
+    z = x / y
+    p = int(payment / z)
+    overpayment = (payment * periods) - p
+    print(f"Your loan principal = {p}!")
+    print(f"Overpayment = {overpayment}")
+
+def annuity_period_calculator(principal, payment, interest):
+    i = interest / 1200
+    total_months = math.ceil(math.log(float(payment) / (float(payment) - i * float(principal)), i + 1))
+    years = total_months // 12
+    months = total_months % 12
+    if months == 0 and years > 1:
+        print(f'It will take {years} years to repay this loan!')
+    elif months == 0 and years == 1:
+        print(f'It will take {years} year to repay this loan!')
+    elif months > 1 and years > 1:
+        print(f"It will take {years} years and {months} months to repay this loan!")
+
+    overpayment = total_months * payment - principal
+    print(f'Overpayment = {overpayment}')
+
+
+if __name__ == '__main__':
+    main()
 
